dd233c6f93c31d1c6549da512d9067d80a2caa10
DKotov <dkotov292@gmail.com>
2022-09-01 00:05:10 +0300

    some changes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use std::collections::HashMap;
+use crate::provider::Provider;
+use clap::{arg, Command};
+use config::Config;
 use std::fs;
-use clap::{arg, Arg, Command};
-use config::{Config, ConfigError};
-use crate::provider::{OpenWeatherProvider, Provider, WeatherApiProvider};
 
 mod provider;
 
 fn main() {
-
     let matches = Command::new("weather")
         .about("test weather app")
         .arg_required_else_help(true)
@@ -22,40 +20,48 @@ fn main() {
         .subcommand(
             Command::new("get")
                 .about("get weather data from previously selected provider")
-                .arg(
-                    arg!(<ADDRESS>).help("address of interest").required(true))
+                .arg(arg!(<ADDRESS>).help("address of interest").required(true))
                 .arg(arg!(--date <DATE>).required(false)),
         )
         .get_matches();
-
     let config = Config::builder()
         .add_source(config::File::with_name("config"))
         .build()
         .unwrap();
     match matches.subcommand() {
         Some(("configure", configure_matches)) => {
-            fs::write("config.toml", format!("provider = \"{}\"\napi_key = \"{}\"",
-                                             configure_matches.value_of("PROVIDER").unwrap(),
-                                             config.get_string("api_key").unwrap()))
-                .expect("Could not write to file!");
+            fs::write(
+                "config.toml",
+                format!(
+                    "provider = \"{}\"\napi_key = \"{}\"",
+                    configure_matches.value_of("PROVIDER").unwrap(),
+                    config.get_string("api_key").unwrap()
+                ),
+            )
+            .expect("Could not write to file!");
         }
         Some(("get", get_matches)) => {
+            let provider_name: String = config
+                .get_string("provider")
+                .expect("unexpected provider in config");
+            let api_key: String = config.get_string("api_key").expect("api key not found");
+            match Provider::try_from((provider_name, api_key)) {
+                Ok(provider_res) => {
+                    match provider_res.get(get_matches.value_of("ADDRESS").unwrap().to_string()) {
+                        Ok(weather_data) => {
+                            println!("{}", weather_data);
+                        }
+                        Err(err) => {
+                            println!("{:?}", err);
+                        }
+                    }
 
-            let mut provider: String = config.get_string("provider").expect("unexpected provider in config");
-
-            match provider.as_str() {
-                "OpenWeather" => {
-                    let prov = OpenWeatherProvider{api_key: config.get_string("api_key").expect("api key not found")};
-                    println!("{}", prov.get(get_matches.value_of("ADDRESS").unwrap().to_string()));
-                },
-                "WeatherApi" => {
-                    let prov = WeatherApiProvider{api_key: config.get_string("api_key").expect("api key not found")};
-                    println!("{}", prov.get(get_matches.value_of("ADDRESS").unwrap().to_string()));
-                },
-                _ => {
-                    panic!("unsupported  provider in config");
+                }
+                Err(e) => {
+                    println!("{:?}", e)
                 }
             }
+
         }
         _ => println!("unpredictable move"),
     };
--- src/provider.rs
+++ src/provider.rs
@@ -1,11 +1,22 @@
-use reqwest::blocking::Response;
+use crate::provider::ProviderCreationError::NotImplemented;
+use crate::provider::ProviderUsingError::{Get};
+use crate::Provider::{OpenWeather, WeatherApi};
+use reqwest::Error;
 use serde_json::Value;
-use std::error::Error;
 use std::fmt::{Display, Formatter};
-use std::io;
 
-pub trait Provider {
-    fn get(&self, address: String) -> WeatherData;
+pub enum Provider {
+    WeatherApi { api_key: String },
+    OpenWeather { api_key: String },
+}
+#[derive(Debug)]
+pub enum ProviderCreationError {
+    NotImplemented,
+}
+#[derive(Debug)]
+pub enum ProviderUsingError {
+    Get(Error),
+    BadResponse(String),
 }
 
 pub struct WeatherData {
@@ -24,60 +35,69 @@ impl Display for WeatherData {
     }
 }
 
-pub struct WeatherApiProvider {
-    pub api_key: String,
-}
-pub struct OpenWeatherProvider {
-    pub api_key: String,
+impl From<Error> for ProviderUsingError {
+    fn from(e: Error) -> Self {
+        Get(e)
+    }
 }
 
-impl Provider for OpenWeatherProvider {
-    fn get(&self, address: String) -> WeatherData {
-        let mut response = reqwest::blocking::get(format!(
-            "http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}",
-            address, self.api_key
-        ))
-        .unwrap();
-        let response_code = response.status();
-
-        let mut value = &response.json::<Value>().unwrap();
-        if response_code.as_u16() != 200 {
-            panic!("{}", value["message"])
-        }
-        response = reqwest::blocking::get(format!(
-            "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&exclude=hourly,daily&appid={}",
-            value[0]["lat"],value[0]["lon"], self.api_key
-        )).unwrap();
-        let mut value = &response.json::<Value>().unwrap();
-        if response_code.as_u16() != 200 {
-            panic!("{}", value["message"])
-        }
-        WeatherData {
-            temp_c: value["current"]["temp"].as_f64().unwrap() as f64,
-            wind_kph: value["current"]["wind_speed"].as_f64().unwrap(),
-            humidity_percent: value["current"]["humidity"].as_u64().unwrap() as u8,
-            clouds_percent: value["current"]["clouds"].as_u64().unwrap() as u8,
+impl TryFrom<(String, String)> for Provider {
+    type Error = ProviderCreationError;
+    fn try_from((provider, api_key): (String, String)) -> Result<Provider, ProviderCreationError> {
+        match provider.as_str() {
+            "OpenWeather" => Ok(OpenWeather { api_key }),
+            "WeatherApi" => Ok(WeatherApi { api_key }),
+            _ => Err(NotImplemented),
         }
     }
 }
-impl Provider for WeatherApiProvider {
-    fn get(&self, address: String) -> WeatherData {
-        let mut response = reqwest::blocking::get(format!(
-            "http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no",
-            self.api_key, address
-        ))
-        .unwrap();
-        let response_code = response.status();
 
-        let value = &response.json::<Value>().unwrap();
-        if response_code.as_u16() != 200 {
-            panic!("{}", value["message"])
-        }
-        WeatherData {
-            temp_c: value["current"]["temp_c"].as_f64().unwrap(),
-            wind_kph: value["current"]["wind_kph"].as_f64().unwrap(),
-            humidity_percent: value["current"]["humidity"].as_u64().unwrap() as u8,
-            clouds_percent: value["current"]["cloud"].as_u64().unwrap() as u8,
+impl Provider {
+    pub fn get(&self, address: String) -> Result<WeatherData, ProviderUsingError> {
+        match self {
+            WeatherApi { api_key } => {
+                let response = reqwest::blocking::get(format!(
+                    "http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no",
+                    api_key, address
+                ))?;
+                let response_status = response.status();
+                let value = response.json::<Value>()?;
+                if !response_status.is_success() {
+                    return Err(ProviderUsingError::BadResponse(value["message"].to_string()));
+                }
+                Ok(WeatherData {
+                    temp_c: value["current"]["temp_c"].as_f64().unwrap(),
+                    wind_kph: value["current"]["wind_kph"].as_f64().unwrap(),
+                    humidity_percent: value["current"]["humidity"].as_u64().unwrap() as u8,
+                    clouds_percent: value["current"]["cloud"].as_u64().unwrap() as u8,
+                })
+            }
+            OpenWeather { api_key } => {
+                let mut response = reqwest::blocking::get(format!(
+                    "http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}",
+                    address, api_key
+                ))?;
+                let mut response_status = response.status();
+                let mut value = response.json::<Value>()?;
+                if !response_status.is_success() {
+                    return Err(ProviderUsingError::BadResponse(value["message"].to_string()));
+                }
+                response = reqwest::blocking::get(format!(
+                    "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&exclude=hourly,daily&appid={}",
+                    value[0]["lat"], value[0]["lon"], api_key
+                ))?;
+                response_status = response.status();
+                value = response.json::<Value>()?;
+                if !response_status.is_success() {
+                    return Err(ProviderUsingError::BadResponse(value["message"].to_string()));
+                }
+                Ok(WeatherData {
+                    temp_c: value["current"]["temp"].as_f64().unwrap() as f64,
+                    wind_kph: value["current"]["wind_speed"].as_f64().unwrap(),
+                    humidity_percent: value["current"]["humidity"].as_u64().unwrap() as u8,
+                    clouds_percent: value["current"]["clouds"].as_u64().unwrap() as u8,
+                })
+            }
         }
     }
 }
